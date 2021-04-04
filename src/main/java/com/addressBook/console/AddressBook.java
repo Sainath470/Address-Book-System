8278228b00209770583533749ec4509288b2cf85
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-05 01:08:44 +0530

    ADDED: Welcome message to the program

--- src/main/java/com/addressBook/console/AddressBook.java
+++ src/main/java/com/addressBook/console/AddressBook.java
@@ -1,13 +1,8 @@
 package com.addressBook.console;
-
 public class AddressBook {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Address Book Program");
     }
-
-
-
-
 }
 
